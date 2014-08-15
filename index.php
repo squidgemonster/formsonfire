dd9aff54b7e863644a6d19b467558aa99aa919d5
David Squidge Kidger <david@squidgeinc.com>
2014-08-15 17:35:14 +0100

    Initial commit - all elements in place, tabbing working, switches animate

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if lt IE 10]>    <html class="no-js oldie" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/,'js');</script>
 <meta charset="utf-8">
@@ -12,232 +10,162 @@
 <meta name="keywords" content="">
 <meta name="author" content="">
 <meta name="robots" content="follow, index">
+<meta name="viewport" content="user-scalable=no,initial-scale=1.0,maximum-scale=1.0">
 
-<link rel="stylesheet" href="style.css?v=1.1">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="style.css?v=1">
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
 
+<section>
 
-<form>
+	<form>
 
-	<fieldset>
+		<fieldset>
 
-		<legend>Form legend goes here...</legend>
+			<legend>Form Legend: Forms on Fire</legend>
 
-		<p class="FoF-Dotted"><span class="req">*</span> Required field</p>
-
-		<div style="display:inline-block; vertical-align:top; width:40%; text-align:left;">
-			<div class="FoF-Dotted">
-				<label for="size_1"><span class="req">*</span> Name</label>
-				<input type="text" name="size" id="size_1" placeholder="Your name..." />
-			</div>
-
-			<div class="FoF-Dotted">
-				<label for="size_1"><span class="req">*</span> Email address</label>
-				<input type="text" name="size" id="size_1" placeholder="Your email address..." />
-			</div>
-
-			<div class="FoF-Dotted">
-				<label for="size_1">Phone number</label>
-				<input type="text" name="size" id="size_1" placeholder="Your phone number..." />
-			</div>
-
-			<div class="FoF-Dotted">
-				<label for="size_1">Address</label>
-				<input type="text" name="size" id="size_1" placeholder="Your address..." />
-			</div>
-		</div>
-
-		<div style="display:inline-block; vertical-align:top; width:40%; text-align:left;">
-			<div class="FoF-Dotted textarea">
-				<label for="size_1">Message</label>
-				<textarea name="something" placeholder="Your message..."></textarea>
-			</div>
-		</div>
-
-	</fieldset>
-
-</form>
-
-
-<form>
-
-	<fieldset>
-		<legend>Form legend goes here...</legend>
-
-		<label for="male">Male</label>
-			<input type="checkbox" name="sex" value="male" id="male" class="FoF" checked />
-
-		<label>Female</label>
-			<input type="checkbox" name="sex" value="female" id="female" class="FoF" checked />
-
-
-		<!-- <div class="FoF on">
-			<div class="FoF-label checkbox">
-				<div class="FoF-checkbox">
-					<div class="FoF-checkbox-selector"></div>
+				<div class="fof-element">
+					<label for="fof_name">Name</label>
+					<input type="text" name="fof_name" id="fof_name" placeholder="Full name" />
 				</div>
-				Label goes here
-			</div>
-		    <input type="checkbox" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-	    <div class="FoF off">
-			<div class="FoF-label checkbox">
-				<div class="FoF-checkbox">
-					<div class="FoF-checkbox-selector"></div>
+				<div class="fof-element">
+					<label for="fof_email">Email address</label>
+					<input type="email" name="fof_email" id="fof_email" placeholder="Email address" />
 				</div>
-				Label goes here
-			</div>
-		    <input type="checkbox" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-	    <div class="FoF disabled on">
-			<div class="FoF-label checkbox">
-				<div class="FoF-checkbox">
-					<div class="FoF-checkbox-selector"></div>
+				<div class="fof-element">
+					<label for="fof_tel">Contact number</label>
+					<input type="tel" name="fof_tel" id="fof_tel" placeholder="Contact phone number" />
 				</div>
-				Label goes here
-			</div>
-		    <input type="checkbox" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-	    <div class="FoF disabled off">
-			<div class="FoF-label checkbox">
-				<div class="FoF-checkbox">
-					<div class="FoF-checkbox-selector"></div>
+				<div class="fof-element">
+					<label for="fof_date">Pick a date</label>
+					<input type="date" name="fof_date" id="fof_date" placeholder="Pick a date" />
 				</div>
-				Label goes here
-			</div>
-		    <input type="checkbox" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-		<div class="FoF on">
-			<div class="FoF-label radio">
-				<div class="FoF-radio">
-					<div class="FoF-radio-selector"></div>
+				<div class="fof-element">
+					<label for="fof_zip">Zip / Postal code</label>
+					<input type="text" name="fof_zip" id="fof_zip" placeholder="Zip / Postal Code" />
 				</div>
-				Label goes here
-			</div>
-		    <input type="radio" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-	    <div class="FoF off">
-			<div class="FoF-label radio">
-				<div class="FoF-radio">
-					<div class="FoF-radio-selector"></div>
+				<div class="fof-element">
+					<label for="fof-select" class="label">Select an option</label>
+						<div class="fof-select">
+							<select id="fof-select">
+								<option value="" disabled selected>Please select one</option>
+								<option>Select this option 01</option>
+								<option>Select this option 02</option>
+								<option>Select this option 03</option>
+								<option>Select this option 04</option>
+							</select>
+						</div>
 				</div>
-				Label goes here...
-			</div>
-		    <input type="radio" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-	    <div class="FoF disabled on">
-			<div class="FoF-label radio">
-				<div class="FoF-radio">
-					<div class="FoF-radio-selector"></div>
+				<div class="fof-element">
+					<label for="fof_checkbox01">
+						Please select this option
+						<input type="checkbox" id="fof_checkbox01" />
+						<i></i>
+					</label>
 				</div>
-				Label goes here...
-			</div>
-		    <input type="radio" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-	     <div class="FoF disabled off">
-			<div class="FoF-label radio">
-				<div class="FoF-radio">
-					<div class="FoF-radio-selector"></div>
+				<div class="fof-element">
+					<label for="fof_checkboxSwitch01">
+						Would you like to turn this on?
+						<input type="checkbox" id="fof_checkboxSwitch01" />
+						<i class="switch"></i>
+					</label>
 				</div>
-				Label goes here...
-			</div>
-		    <input type="radio" name="size" id="size_1" value="small" />
-		    <label for="size_1">Small</label>
-	    </div>
 
-	    <div class="FoF required">
-	    	<div class="FoF-label text">Label goes here...</div>
-	    		<span class="FoF-text-border">
-		    		<input type="text" name="size" id="size_1" placeholder="Label goes here..." />
-		    	</span>
-		    <label for="size_1">Small</label>
-	    </div>
+				<div class="fof-element">
+					<label for="fof_checkboxSwitch02">
+						Would you like to turn this on?
+						<input type="checkbox" id="fof_checkboxSwitch02" checked />
+						<i class="switch"></i>
+					</label>
+				</div>
 
-	    <div class="FoF focus">
-	    	<div class="FoF-label text">Label goes here...</div>
-	    		<span class="FoF-text-border">
-		    		<input type="text" name="size" id="size_1" placeholder="Label goes here..." />
-		    	</span>
-		    <label for="size_1">Small</label>
-	    </div>
+				<div class="fof-element">
+					<label for="fof_checkboxSwitch03">
+						Would you like to turn this on?
+						<input type="checkbox" id="fof_checkboxSwitch03" checked disabled />
+						<i class="switch"></i>
+					</label>
+				</div>
 
-	    <div class="FoF disabled">
-	    	<div class="FoF-label text">Label goes here...</div>
-	    		<span class="FoF-text-border">
-		    		<input type="text" name="size" id="size_1" placeholder="Label goes here..." />
-		    	</span>
-		    <label for="size_1">Small</label>
-	    </div> -->
+				<div class="fof-element list">
+					<span class="label">Please select all that apply</span>
+					<label for="fof_checkbox02">
+						<input type="checkbox" id="fof_checkbox02" />
+						<i></i>
+						Option 01
+					</label>
+					<label for="fof_checkbox03">
+						<input type="checkbox" id="fof_checkbox03" />
+						<i></i>
+						Option 02
+					</label>
+					<label for="fof_checkbox04">
+						<input type="checkbox" id="fof_checkbox04" />
+						<i></i>
+						Option 03
+					</label>
+					<label for="fof_checkbox05">
+						<input type="checkbox" id="fof_checkbox05" />
+						<i></i>
+						Option 05
+					</label>
+				</div>
 
-	</fieldset>
+				<div class="fof-element">
+					<span class="label">Would you agree?</span>
+					<label>
+						Yes
+						<input type="radio" name="fof_radio01" value="yes" />
+						<i></i>
+					</label>
+					<label>
+						No
+						<input type="radio" name="fof_radio01" value="no" />
+						<i></i>
+					</label>
+					<label>
+						Don't know
+						<input type="radio" name="fof_radio01" value="no" />
+						<i></i>
+					</label>
+				</div>
 
-</form>
+				<div class="fof-element list">
+					<span class="label">Please select one option</span>
+					<label>
+						<input type="radio" name="fof_radio02" value="1" />
+						<i></i>
+						Select one option
+					</label>
+					<label>
+						<input type="radio" name="fof_radio02" value="2" />
+						<i></i>
+						Select one option
+					</label>
+					<label>
+						<input type="radio" name="fof_radio02" value="3" />
+						<i></i>
+						Select one option
+					</label>
+				</div>
 
-<script src="forms-on-fire.js" />
-<script>
-$(document).ready(function () { 
+				<button>Go go go!</button>
 
-	// $('.toggler').live("click", function(){
+		</fieldset>
 
-	// 	if($(this).hasClass('on')) {
-	// 		$('.slider').animate({"left": "-40px"}, "slow");
-	// 	} else {
-	// 		$('.slider').animate({"left": "40px"}, "slow");
-	// 	}
+	</form>
 
-	// });
+</section>
 
-	$(document).on("click", ".toggler", function(){ 
-
-		if($(this).hasClass('on')) {
-			$(this).find('.slider').animate({"left": "-40px"}, "fast");
-			$(this).addClass('off');
-			$(this).removeClass('on');
-		} else {
-			$(this).find('.slider').animate({"left": "0"}, "fast");
-			$(this).addClass('on');
-			$(this).removeClass('off');
-		}
-
-	});
-
-	$('.FoF-Dotted input').focus(function(){
-
-		$(this).attr('placeholder', '');
-		$(this).prev('label').fadeIn('slow');
-		$(this).addClass('on');
-
-	});
-
-	$('.notification-trigger').click(function(){
-		$('#notification').show();
-		$('#notification').animate({'opacity': 0}, 0);
-		$('#notification').animate({'opacity': 1,  'bottom': '50%'}, 450).delay(2250).animate({'opacity':0, 'bottom':'100%'}, 800, function() {
-  			$('#notification').removeAttr('style');});
-		// $('#notification').attr('style','');
-		return false;
-	});
-
-});
-</script>
 </body>
 </html>
