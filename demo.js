fc028e7b67245bb4800ca29830c18b2bd8b341c6
David Squidge Kidger <david@squidgeinc.com>
2014-09-03 18:17:45 +0100

    Separated out Javascript to individual file

---
+++ demo.js
@@ -0,0 +1,7 @@
+// Prepoulate your date field
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+document.getElementById('fof_date').value = new Date().toDateInputValue();
